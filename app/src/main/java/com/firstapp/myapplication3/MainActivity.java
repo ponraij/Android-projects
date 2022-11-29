730970b1be168a9e948bcb4c85fae3068332d7c1
ponraj <ponraij@gmail.com>
2022-11-29 14:41:20 +0530

    login android app

---
+++ app/src/main/java/com/firstapp/myapplication3/MainActivity.java
@@ -0,0 +1,39 @@
+package com.firstapp.myapplication3;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username =(TextView)findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton btn = (MaterialButton) findViewById(R.id.btn);
+
+        //admin and admin
+
+        btn.setOnClickListener(v -> {
+
+            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                //correct
+                Toast.makeText(MainActivity.this,"successful",Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                //incorrect
+                Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
