acd6951d59018a062cb8438947e8c86cbc44cc4c
ponraj <ponraij@gmail.com>
2022-11-29 16:34:50 +0530

    admin and user pages created

--- app/src/main/java/com/firstapp/myapplication3/MainActivity.java
+++ app/src/main/java/com/firstapp/myapplication3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.firstapp.myapplication3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,10 +12,13 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialButton button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn);
 
         TextView username =(TextView)findViewById(R.id.username);
         TextView password =(TextView) findViewById(R.id.password);
@@ -22,16 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
         //admin and admin
 
-        btn.setOnClickListener(v -> {
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                //correct
-                Toast.makeText(MainActivity.this,"successful",Toast.LENGTH_SHORT).show();
-            }
+                Intent intent1 = new Intent(MainActivity.this, AdminActivity.class);
+                Intent intent2 =  new Intent(MainActivity.this,UserActivity.class);
 
-            else {
-                //incorrect
-                Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+                    startActivity(intent1);
+                }
+
+                else if(username.getText().toString().equals("user") && password.getText().toString().equals("user")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "login sucessful", Toast.LENGTH_SHORT).show();
+                    startActivity(intent2);
+                }
+
+                else {
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
